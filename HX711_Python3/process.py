fae706741f120773041d122466c7440ceb0bb19c
Timothy W <timothywee2@gmail.com>
2022-03-25 20:20:00 +0800

    Load cell implementation
    1. Added HX711_Python3 library for load cell
    2. Added process.py, capturing the processes to initialise and read from load cell.
    
    Variables:
    - scale_input: raw readout from calibration program after weight is placed (currently 48739)
    - weight: weight of object placed (currently 100g)

---
+++ HX711_Python3/process.py
@@ -0,0 +1,41 @@
+scale_input = 48739
+weight = 100
+ratio = float(weight/scale_input)
+
+#Libraries for HX711 load cell:
+import RPi.GPIO as GPIO  # import GPIO
+from hx711 import HX711  # import the class HX711
+from hx711 import outliers_filter
+
+#Load cell setup start
+
+GPIO.setmode(GPIO.BCM)  # set GPIO pin mode to BCM numbering
+    # Create an object hx which represents your real hx711 chip
+    # Required input parameters are only 'dout_pin' and 'pd_sck_pin'
+
+hx = HX711(dout_pin=21, pd_sck_pin=20)
+    # measure tare and save the value as offset for current channel
+    # and gain selected. That means channel A and gain 128
+    #CHANNEL A
+    #GAIN 128
+
+err = hx.zero()
+if err:
+    raise ValueError('Tare is unsuccessful.')
+    #checks for error in initialising HX711
+
+hx.set_scale_ratio(ratio)
+#set ratio used to convert load cell readings into grams
+
+# print(hx.get_weight_mean(20), 'g') #prints the weight in grams. Loop this 
+
+while not KeyboardInterrupt:
+    print(hx.get_weight_mean(20), 'g')
+
+
+
+#end of code
+# Cleaning up the ports that we have used in this code. It is important to do so,
+# otherwise you might have problems when you want to use the same ports for another
+# application.
+GPIO.cleanup()
